56fd04e7c41a3d93e6fa2c1605951bb62e565fda
mytev <tevainui.miyou@gmail.com>
2016-06-15 13:09:44 -0400

    Adding algorithme aleatoire et tabu

--- Algorithme.java
+++ Algorithme.java
@@ -82,7 +82,7 @@ public class Algorithme {
 				//on shuffle de 5 mouvements
 				shuffle_count++;
 				if(shuffle_count > 10){
-					tmpSolu.shuffle(4);
+					tmpSolu.shuffle(5);
 					shuffle_count=0;
 				}
 			}
